e5356a3d9956355026422348af5863c45f521652
Maria Abdallah <maria.safwat2018@gmail.com>
2023-08-22 00:31:34 -0700

    printf

--- main.h
+++ main.h
@@ -1,16 +1,88 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _PRINTF_H
+#define _PRINTF_H
+
 #include <stdarg.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
 
-#define UNUSED(x) (void)(x)
+#define OUTPUT_BUF_SIZE 1024
+#define BUF_FLUSH -1
 
+
+#define NULL_STRING "(null)"
+
+#define PAR_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
+
+/**
+ * struct parameters - parameters struct
+ *
+ * @unsign: flag if unsigned value
+ *
+ * @plus_flag: on if plus_flag specified
+ * @space_flag: on if hashtag_flag specified
+ * @hashtag_flag: on if _flag specified
+ * @zero_flag: on if _flag specified
+ * @minus_flag: on if _flag specified
+ *
+ * @width: field width specified
+ * @precision: field precision specified
+ *
+ * @h_modifier: on if h_modifier is specified
+ * @l_modifier: on if l_modifier is specified
+ *
+ */
+typedef struct parameters
+{
+	unsigned int unsign			: 1;
+
+	unsigned int plus_flag		: 1;
+	unsigned int space_flag		: 1;
+	unsigned int hashtag_flag	: 1;
+	unsigned int zero_flag		: 1;
+	unsigned int minus_flag		: 1;
+
+	unsigned int width;
+	unsigned int precision;
+
+	unsigned int h_modifier		: 1;
+	unsigned int l_modifier		: 1;
+} par;
+
+/**
+ * struct specifier - Struct token
+ *
+ * @specifier: format token
+ * @f: The function associated
+ */
+typedef struct specifier
+{
+	char *specifier;
+	int (*f)(va_list, par *);
+} specifier_t;
+
+/* _put.c module */
+int _puts(char *str);
+int _putchar(int c);
+
+/* print_functions.c module */
+int print_char(va_list ap, params_t *params);
+int print_int(va_list ap, params_t *params);
+int print_string(va_list ap, params_t *params);
+int print_percent(va_list ap, params_t *params);
+int print_S(va_list ap, params_t *params);
+
+/* specifier.c module */
+int (*get_specifier(char *s))(va_list ap, par *pa);
+int get_print_func(char *s, va_list ap, par *pa);
+int print_from_to(char *start, char *stop, char *except);
+void init_par(par *pa, va_list ap);
+
+/* _prinf.c module */
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i, va_list list);
 
-/*functions to print chars and strings */
-int print_char(va_list types);
-int print_string(va_list types);
-int print_percent(va_list types);
-
-#endif /* MAIN_H */
+#endif
